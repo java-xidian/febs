3c4ebc19ac957cb0cebc69210f91ee691d31ab2d
liuchangqi <liucq@fapiao.com>
2019-03-06 19:00:23 +0800

    代理对象角色

---
+++ src/main/java/com/mrbird/api/module/design12Proxy/Client.java
@@ -0,0 +1,16 @@
+package com.mrbird.api.module.design12Proxy;
+
+/**
+ * @Description
+ * @Author: liucq
+ * @Date: 2019/3/6
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        AbstractObject obj = new ProxyObject();
+        obj.operation();
+    }
+
+}
