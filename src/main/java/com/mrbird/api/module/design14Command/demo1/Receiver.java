a4f4a23b184896bc7dcc8151b866d48a1e857c87
liucq <1650424496@qq.com>
2019-03-10 22:16:09 +0800

    rebuild

---
+++ src/main/java/com/mrbird/api/module/design14Command/demo1/Receiver.java
@@ -0,0 +1,13 @@
+package com.mrbird.api.module.design14Command.demo1;
+
+/**
+ * 接收者角色类
+ */
+public class Receiver {
+    /**
+     * 真正执行命令相应的操作
+     */
+    public void action(){
+        System.out.println("执行操作");
+    }
+}
