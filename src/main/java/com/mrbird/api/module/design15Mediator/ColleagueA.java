0e75f5cfeca171195755b7c031626fb078472a9b
liuchangqi <liucq@fapiao.com>
2019-03-13 14:30:37 +0800

    代理对象角色

--- src/main/java/com/mrbird/api/module/design15Mediator/ColleagueA.java
+++
@@ -1,8 +0,0 @@
-package com.mrbird.api.module.design15Mediator;
-
-class ColleagueA extends AbstractColleague{
-    public void setNumber(int number, AbstractColleague coll) {
-        this.number = number;
-        coll.setNumber(number*100);
-    }
-}
