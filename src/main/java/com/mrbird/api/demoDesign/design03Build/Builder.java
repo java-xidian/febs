0bffd6775617825430ee4198283907650cdd0370
liuchangqi <liucq@fapiao.com>
2019-04-09 16:45:10 +0800

    callable

--- src/main/java/com/mrbird/api/demoDesign/design03Build/Builder.java
+++ src/main/java/com/mrbird/api/demoDesign/design03Build/Builder.java
@@ -1,11 +1,14 @@
 package com.mrbird.api.demoDesign.design03Build;
 
 /**
+ * 抽象建造者类Builder
+ *
  * @Description
  * @Author: liucq
  * @Date: 2019/3/1
  */
 public interface Builder {
+
     public void buildPart1();
 
     public void buildPart2();
